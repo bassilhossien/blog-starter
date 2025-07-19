c38cf51340a49733637443c70bfa85d34ebeecdf
Bassel <bassil.hossien@gmail.com>
2025-07-18 22:51:15 -0600

    customized home page

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -4,7 +4,7 @@ export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        Customized Blog.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         A statically generated blog example using{" "}
@@ -15,6 +15,9 @@ export function Intro() {
           Next.js
         </a>{" "}
         and {CMS_NAME}.
+        <div className="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4" role="alert">
+        <p className="warning">Customized by: <b>Bassel Al Hussein</b> for the purpose of an assessment.</p>
+        </div>
       </h4>
     </section>
   );
